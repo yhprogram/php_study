f075fcecc11fb17c51ce333cc7f46b06d88b483a
yhprogram <45761247+yhprogram@users.noreply.github.com>
2018-12-11 23:09:38 +0900

    Add files via upload

---
+++ samaple01.php
@@ -0,0 +1,16 @@
+<?php
+    $price = 12300;
+    $total = $price * 1.05;
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>sample page</title>
+</head>
+<body>
+    <?php echo "PHPによるテキストです。"; ?>
+</body>
+</html>
